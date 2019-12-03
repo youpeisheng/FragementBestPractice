db055e9a5f0a2b48855431449c03c7d8544d3be5
youpeisheng <1426890389@qq.com>
2019-12-03 09:49:29 +0800

    增加了注释

--- app/src/main/java/com/example/fragementbestpractice/NewsContentActivity.java
+++ app/src/main/java/com/example/fragementbestpractice/NewsContentActivity.java
@@ -22,5 +22,4 @@ public class NewsContentActivity extends AppCompatActivity {
         NewsContentFragment newsContentFragment =(NewsContentFragment) getSupportFragmentManager().findFragmentById(R.id.news_content_fragment);
         newsContentFragment.refresh(newsTitle,newsContent);//刷新 界面
     }
-
 }
